e04222fed767d9dfa26d275bc0014f112dd9a7fe
William Torres <39351781+williamtorres1@users.noreply.github.com>
2019-11-28 11:37:26 -0300

    Update #5 - IF e Else (Estrutura de decisão e alternativa).py

--- Python/Coursera/#5 - IF e Else (Estrutura de decisão e alternativa).py
+++ Python/Coursera/#5 - IF e Else (Estrutura de decisão e alternativa).py
@@ -3,6 +3,6 @@ peso = float(input('Digite seu peso, em kgs: '))
 idade = int(input('Digite sua iade: '))
 IMC = peso / altura ** 2
 if altura < 3.0 and altura > 0.3 and peso > 0 and peso < 550:
-    print('Você tem {} anos, e seu IMD é {:.2f}'.format(idade,IMC))
+    print('Você tem {} anos, e seu IMC é {:.2f}'.format(idade,IMC))
 else:
     print('Voce digitou algum valor errado, o programa ira encerrar...')

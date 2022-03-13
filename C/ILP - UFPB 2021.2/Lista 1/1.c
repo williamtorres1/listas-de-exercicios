096a38ab5634fbd9123439f21810e84df4c5b3c7
William Torres <contatowilliamtorres@gmail.com>
2022-03-12 21:13:28 -0300

    Resolvendo problemas de indentação

--- C/ILP - UFPB 2021.2/Lista 1/1.c
+++ C/ILP - UFPB 2021.2/Lista 1/1.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 int main()
 {
-	/*
-    Nesse primeiro exercicio de laboratorio vamos desenvolver um programa que devera
-      ler tres notas distintas de um aluno e ao final exibir a media aritmetica dessas notas.
-	*/
-	float firstGrade = 0, secondGrade = 0, thirdGrade = 0, average = 0;
-	printf("Este programa irá calcular a média de três notas.\n");
-	printf("Adicione a primeira nota: ");
-	scanf("%f", &firstGrade);
-	printf("Adicione a segunda nota: ");
-	scanf("%f", &secondGrade);
-	printf("Adicione a terceira nota: ");
-	scanf("%f", &thirdGrade);
+  /*
+  Nesse primeiro exercicio de laboratorio vamos desenvolver um programa que devera
+    ler tres notas distintas de um aluno e ao final exibir a media aritmetica dessas notas.
+  */
+  float firstGrade = 0, secondGrade = 0, thirdGrade = 0, average = 0;
+  printf("Este programa irá calcular a média de três notas.\n");
+  printf("Adicione a primeira nota: ");
+  scanf("%f", &firstGrade);
+  printf("Adicione a segunda nota: ");
+  scanf("%f", &secondGrade);
+  printf("Adicione a terceira nota: ");
+  scanf("%f", &thirdGrade);
 
-	printf("Calculando...\n");
-	average = (firstGrade + secondGrade + thirdGrade)/3;
-	printf("A média é: %.3f\n", average);
-	return 1;
+  printf("Calculando...\n");
+  average = (firstGrade + secondGrade + thirdGrade)/3;
+  printf("A média é: %.3f\n", average);
+  return 1;
 }
--- C/ILP - UFPB 2021.2/Lista 1/2.c
+++ C/ILP - UFPB 2021.2/Lista 1/2.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 int main()
 {
-	/*
-		Para esse exercicio, nos vamos implementar um programa que calcula e exibe os valores
+  /*
+    Para esse exercicio, nos vamos implementar um programa que calcula e exibe os valores
       do raio, da area e da circunferencia de um circulo. Tais valores devem ser calculados a
       partir do diametro do circulo, que deve ser lido do teclado.
-	*/
-	float circumference = 0, radius = 0, area = 0, diameter = 0;
-	float pi = 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128481117450284102701938521105559644622948954930381964428810975665933446128475648233786783165271201909145648566923460348610454326648213393607260249141273;
+  */
+  float circumference = 0, radius = 0, area = 0, diameter = 0;
+  float pi = 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128481117450284102701938521105559644622948954930381964428810975665933446128475648233786783165271201909145648566923460348610454326648213393607260249141273;
 
-	printf("Este programa irá calcular a circunferência, raio e área a partir do diâmetro.\n");
-	printf("O valor de pi utilizado está com 300 casas decimais, fonte: WikiPedia.\n\n");
+  printf("Este programa irá calcular a circunferência, raio e área a partir do diâmetro.\n");
+  printf("O valor de pi utilizado está com 300 casas decimais, fonte: WikiPedia.\n\n");
 
-	printf("Adicione o valor do diâmetro, em metros: ");
-	scanf("%f", &diameter);
+  printf("Adicione o valor do diâmetro, em metros: ");
+  scanf("%f", &diameter);
 
-	printf("Calculando a circunferência...\n");
-	circumference = pi * diameter;
+  printf("Calculando a circunferência...\n");
+  circumference = pi * diameter;
 
   printf("Calculando o raio...\n");
   radius = diameter/2;
@@ -26,5 +26,5 @@ int main()
 
   printf("Valores obtidos, todos em metros:\n");
   printf("Circunferência: %.3fm\nRaio: %.3fm\nDiâmetro: %.3fm\nÁrea: %.3fm\n", circumference, radius, diameter, area);
-	return 1;
+  return 1;
 }

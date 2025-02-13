7ca299419d3152e25702e491bdc4e3f6be87cc90
William Torres <contatowilliamtorres@gmail.com>
2025-02-13 11:30:50 -0300

    2ª Atividade 13/02, calculadora com switch-case

---
+++ C/ILP - UFPB 2024.2/#5 - calculator.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+  char operator = '+';
+  float numberOne, numberTwo = 0;
+  printf("Digite qual operação quer realizar:\n+)\n-)\n*)\n/)");
+  operator = getchar();
+  printf("Digite os dois valores para serem calculados: ");
+  scanf("%f %f", &numberOne, &numberTwo);
+  switch (operator)
+  {
+  case '+':
+    printf("A soma é: %.2f", (numberOne + numberTwo));
+    break;
+  case '-':
+    printf("A subtração é: %.2f", (numberOne - numberTwo));
+  break;
+  case '*':
+    printf("A multiplicação é: %.2f", (numberOne * numberTwo));
+  break;
+  case '/':
+    printf("A divisão é: %.2f", (numberOne/numberTwo));
+  break;
+  default:
+    printf("Caracter inválido!");
+    break;
+  }
+  return 0;
+}

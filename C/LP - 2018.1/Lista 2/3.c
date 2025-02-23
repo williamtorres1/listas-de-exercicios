149249cbc66a31819c51b61cabe9331cd2eead96
William Torres <contatowilliamtorres@gmail.com>
2025-02-23 19:45:36 -0300

    Exercício 3 da lista 2 de 2018.

--- C/LP - 2018.1/Lista 2/3.c
+++ C/LP - 2018.1/Lista 2/3.c
@@ -9,6 +9,15 @@ int main()
 		Dados o peso do saco de ração e a quantidade de ração fornecida para cada 
 			gato, calcule quanto restará de ração no saco após cinco dias.
 */
+	int rationInKilograms, rationForEachCat;
+	float rationLeftAfterFiveDays;
+	printf("Qual a quantidade de ração para os gatos, em Kgs? ");
+	scanf("%d", &rationInKilograms);
+	printf("Qual a quantidade de ração para os gatos, em gramas, individualmente? ");
+	scanf("%d", &rationForEachCat);
+	rationLeftAfterFiveDays = (rationInKilograms * 1000 - rationForEachCat * 2 * 5);
+	printf("A quantidade de ração restante após 5 dias é: %.2f Kgs.\n", 
+	rationLeftAfterFiveDays/1000);
 	system("pause");
 	return 0;
 }

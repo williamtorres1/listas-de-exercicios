f829d10f3d700fe4ba873f26c6e4e068f1d82f30
William Torres <contatowilliamtorres@gmail.com>
2025-04-10 00:17:28 -0300

    Exercício de struct com geração de arquivo

---
+++ C/ILP - UFPB 2024.2/#31 - struct.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+
+#define NAMESIZE 50
+#define CARGOSIZE 50
+#define QUANTIDADEALUNOS 100
+
+typedef enum {
+  APROVADO,
+  REPROVADO
+} alunoSituacao;
+
+typedef struct {
+  char nome[NAMESIZE];
+  float media;
+  alunoSituacao situacao;
+} dadosAlunos;
+
+typedef struct {
+  unsigned int aprovadosQuantidade;
+  unsigned int reprovadosQuantidade;
+  float mediaTurma;
+  float percentagemAprovados;
+} dadosTurma;
+
+void cadastrarAlunos(dadosAlunos *alunos, int *alunosRegistrados){
+  float nota1, nota2;
+  getchar();//Limpar buffer do teclado
+  printf("Digite o nome do aluno: ");
+  fgets(alunos[*alunosRegistrados].nome, NAMESIZE, stdin);
+  printf("Digite as duas notas do aluno: ");
+  scanf("%f %f", &nota1, &nota2);
+  alunos[*alunosRegistrados].media = (nota1 + nota2)/2;
+  if (alunos[*alunosRegistrados].media >= 7){
+    alunos[*alunosRegistrados].situacao = APROVADO;
+  }
+  else alunos[*alunosRegistrados].situacao = REPROVADO;
+  (*alunosRegistrados)++;
+}
+
+void relatorioDaTurma(dadosAlunos *alunos, int alunosRegistrados){
+  dadosTurma turma;
+  turma.mediaTurma = 0;
+  turma.aprovadosQuantidade = 0;
+  turma.reprovadosQuantidade = 0;
+  turma.percentagemAprovados = 0;
+  char string_final[100];
+
+  int i;
+
+  for (i = 0; i < alunosRegistrados; i++){
+    if (alunos[i].situacao == 0){
+      turma.aprovadosQuantidade = turma.aprovadosQuantidade + 1;
+    }
+    else if (alunos[i].situacao == 1) {
+      turma.reprovadosQuantidade = turma.reprovadosQuantidade + 1;
+    }
+    turma.mediaTurma += alunos[i].media;
+  }
+  turma.percentagemAprovados = 100 * ((float) turma.aprovadosQuantidade / alunosRegistrados);
+  turma.mediaTurma = turma.mediaTurma / alunosRegistrados;
+
+  sprintf(string_final, "\n\t Relatório da turma!\n"
+  "\nMédia da turma: %.2f \nAprovados: %d | Reprovados: %d | percentagem de Aprovados: %.2f%% \n", 
+    turma.mediaTurma,
+    turma.aprovadosQuantidade,
+    turma.reprovadosQuantidade,
+    turma.percentagemAprovados);
+  
+  printf("%s", string_final);
+
+  FILE *relatorio;
+  relatorio = fopen("relatorio.txt", "wb");
+  if (relatorio == NULL){
+    printf("Erro! Arquivo não criado!");
+    return;
+  }
+  if(fputs(string_final, relatorio) == -1)
+    printf("Erro! Arquivo não salvo!");
+  fclose(relatorio);
+}
+
+int main(){
+   /*
+     Faça um programa que receba, via teclado, o nome e as duas notas dos alunos
+     a) Armazene essas informações em um Array de Structs.
+     b) faça um relatório contendo: 
+       Média da classe; 
+       Quantidade de aprovados;
+       Quantidade de reprovados; 
+       percentagem de aprovados.
+     Esse relatório deve ser salvo em um arquivo relatorio.txt.
+     Opcional: Gere também um arquivo txt com os dados digitados no formato 
+       nome1, nota 1, nota2
+   */
+   dadosAlunos alunos[QUANTIDADEALUNOS];
+   int escolha = 0, alunosRegistrados = 0;
+   while (1){
+    printf("Digite o que quer fazer: \n1)Cadastrar aluno \n2)Relatório da turma\n");
+    scanf("%d", &escolha);
+    if (escolha == 1) {
+      cadastrarAlunos(alunos, &alunosRegistrados);
+     } else if (escolha == 2) {
+      relatorioDaTurma(alunos, alunosRegistrados);
+     }
+    else return 0;
+   }
+   return 0;
+}

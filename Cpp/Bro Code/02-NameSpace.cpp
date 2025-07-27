915f6c9e89d862efa45ddee8de5cedd8849ea395
William Torres <contatowilliamtorres@gmail.com>
2025-07-27 12:08:27 -0300

    Diferença entre typedef e using para criar tipos complexos de dados

--- Cpp/Bro Code/02-NameSpace.cpp
+++ Cpp/Bro Code/02-NameSpace.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 namespace first{
   int x = 1;
 }
@@ -8,9 +8,26 @@ namespace second{
   int x = 2;
 }
 
+// Typedef cria um alias para um outro tipo de dados, similar ao struct no C
+typedef std::vector<std::pair<std::string, int>> pairlist_t;
+typedef std::string text_t;
+//typedef int number_t;
+
+// Outra forma de fazer, funciona melhor com templates (mais recomendável).
+//using text_t = std::string;
+using number_t = int;
+
 int main(){
   int x = 30;
 
   using namespace std;
-  cout << second::x;
+  cout << second::x << endl;
+
+  text_t firstName = "William";
+  cout << firstName << endl;
+
+  number_t age = 24;
+  cout << age << endl;
+
+  return 0;
 }

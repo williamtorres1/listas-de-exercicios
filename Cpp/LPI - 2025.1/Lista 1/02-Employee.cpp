8552f8a72f949ae70b5b3c2a26da1e9cf250871c
William Torres <contatowilliamtorres@gmail.com>
2025-07-28 22:59:23 -0300

    4º exercício de constructors - Funcionários 02

---
+++ Cpp/LPI - 2025.1/Lista 1/02-Employee.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "02-Employee.h"
+
+int main(){
+  unsigned int n;
+  std::cin >> n;
+  Employees *employee = new Employees[n];
+
+  for(unsigned int i = 0; i < n; i++){
+      std::string value;
+      float salary;
+      std::getline(std::cin >> std::ws, value);
+      employee[i].setFirstName(value);
+      std::getline(std::cin >> std::ws,value);
+      employee[i].setLastName(value);
+
+      std::cin >> salary;
+      employee[i].setWage(salary);
+      std::cin.ignore();
+  }
+  for (unsigned int i = 0; i < n; i++){
+    std::cout << employee[i].getFirstName() << ' ' << employee[i].getLastName() << " - " << employee[i].getWage() << " - " << employee[i].getAnualWage() << '\n';
+    std::cout << employee[i].increaseWage(0.1) << std::endl;
+  }
+  return 0;
+}

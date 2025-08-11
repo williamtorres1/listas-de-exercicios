bc9521ef204431b7c3a58c1d8b661c4b69a6ee57
William Torres <contatowilliamtorres@gmail.com>
2025-08-11 09:42:52 -0300

    adiciona exercícios de arquivo (somente escopo)

---
+++ C/ILP - UFPB 2024.2/24-arquivos.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+  /*
+  * Faça um programa que usa a função fgetc() e fputc() para abrir um
+      arquivo de texto e substituir os caracteres:
+      - 'a' por 4,
+      - 'e' por 3,
+      - 'i' por 1, 
+      - 'o' por 0,
+      - 'u' por 5
+  */
+}
---
+++ C/ILP - UFPB 2024.2/25-arquivos.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+  /*
+    Faça um programa que receba de um arquivo .txt com dados de
+      alunos no seguinte formato:
+      - nome1, nota1, nota2
+      - nome1, nota1, nota2
+    O programa deve elaborar um relatório em outro arquivo com 
+      a lista de nomes, média de duas notas de cada aluno e
+      situação: aprovado, na final ou reprovado.
+    No final do arquivo informe a porcentagem dos alunos aprovados
+      e média da turma.
+  */
+}
